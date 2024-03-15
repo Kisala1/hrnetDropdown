673d5315c7d4b7735544dc7e34adc5af8f8d4ed5
Aude Alexandre <alexandre.aude16@gmail.com>
2024-03-15 21:50:10 +0100

    Création composant

---
+++ dropdown-component-library/src/lib/Dropdown.jsx
@@ -0,0 +1,37 @@
+export function Dropdown({
+  name,
+  options,
+  containerStyle,
+  labelStyle,
+  selectStyle,
+}) {
+  const styles = {
+    container: containerStyle || {
+      display: 'flex',
+      flexDirection: 'column',
+      margin: '10px 0',
+    },
+    label: labelStyle || {
+      marginBottom: '10px',
+    },
+    select: selectStyle || {
+      cursor: 'pointer',
+      padding: '5px 10px',
+      fontSize: 'medium',
+      backgroundColor: '#f6f6f6',
+      borderRadius: '5px',
+    },
+  };
+  return (
+    <div style={styles.container}>
+      <label htmlFor={name} style={styles.label}>
+        {name}
+      </label>
+      <select name={name} id={name} style={styles.select}>
+        {options.map((option, index) => {
+          return <option key={index}>{option}</option>;
+        })}
+      </select>
+    </div>
+  );
+}
